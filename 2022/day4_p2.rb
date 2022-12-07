b000f47fa5fdcb382e162204cf21447af975cdb2
Sarah Pickett <110683078+sqrlington@users.noreply.github.com>
2022-12-06 21:16:33 -0500

    Delete day4_p2.rb

--- 2022/day4_p2.rb
+++
@@ -1,8 +0,0 @@
-input = File.read("day4.txt").split("\n").map {|pairing| pairing.split(",")}.map do |ranges|
-   range1 = Range.new(ranges[0].split("-").map(&:to_i).first,ranges[0].split("-").map(&:to_i).last).to_a
-   range2 = Range.new(ranges[1].split("-").map(&:to_i).first,ranges[1].split("-").map(&:to_i).last).to_a
-   (range1 & range2).empty?
-end
-
-puts input.count(false)
-
