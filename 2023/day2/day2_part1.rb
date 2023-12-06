600a5a826896e0f78b9518d6774f3e9f800eb50d
Sarah Pickett <sarahpickett@shopify.com>
2023-12-06 17:05:14 -0500

    minor rubyism updates

--- 2023/day2/day2_part1.rb
+++ 2023/day2/day2_part1.rb
@@ -1,33 +1,27 @@
-games = File.open("input.txt").readlines.map(&:chomp).map do |line|
+games = File.open("day2_sample_input.txt").readlines.map(&:chomp).map do |line|
  line.split(/(:|;)/).grep_v(/(:|;)/)
 end.compact
 
-def get_color_value(set, color)
-  match = set.match(/(\d+)\s#{color}/)
-  if match.nil?
-    0
-  else
-    match[0].to_i
+def get_rgb_values(set)
+  ["red", "green", "blue"].map do |color|
+    match = set.match(/(\d+)\s#{color}/)
+    match.nil? ? 0 : match[0].to_i
   end
 end
 
 
-def is_possible?(sets)
+def possible?(sets)
   result = sets.map do |set|
-    red = get_color_value(set, "red")
-    green = get_color_value(set, "green")
-    blue = get_color_value(set, "blue")
-
+    red, green, blue = get_rgb_values(set)
     # magic numbers! :shrug:
     green <= 13 && red <= 12 && blue <= 14
   end.all?
 end
 
-sum = 0
 
-games.each do |game|
+sum = games.map do |game|
   game_number = game.shift.match(/(\d+)/)[1].to_i
-  sum = sum + game_number if is_possible?(game)
-end
+  possible?(game) ? game_number : 0
+end.sum
 
 puts sum
