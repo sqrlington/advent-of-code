e1b0a58e9628f5a296bb858a97d0dbfd0a829ae3
Sarah Pickett <sarah.pickett@shopify.com>
2025-12-03 18:01:46 -0500

    day 2 ✅

---
+++ 2025/day2/day2_part1.rb
@@ -0,0 +1,9 @@
+input = File.read("puzzle_input.txt", chomp: true).split(",").map do |range|
+  range.split("-").map(&:to_i)
+end
+
+invalid = input.sum do |start, finish|
+  (start..finish).select { |num| num.to_s.match?(/^(\d+)\1$/) }.sum
+end
+
+puts invalid
